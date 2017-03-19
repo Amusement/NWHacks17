75085378ffa3453bdb414dbcadc69cffcd42e234
Кристина Ильовска < >
2017-03-18 22:41:22 -0700

    Tabs info

---
+++ tabScript.js
@@ -0,0 +1,23 @@
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+	chrome.tabs.query({active: false}, function(tabs) {
+		var urls = "The urls are:<br />";
+		tabs.forEach(function(item, index, array){
+			urls += "Tab #" + index + ": url: " + item.url + "<br />";
+			urls += "The content settings are: <br />";
+			chrome.contentSettings['images'].get({primaryUrl: item.url}, function (details){
+
+				//This will give you whether the setting is allowed/blocked:
+				// alert("The images setting for " + item.url + " is " + details.setting);
+			});
+
+		});
+		document.write(urls);
+	});
+
+});
